aad4c8f3bc8c4b50a473e50a21b104b68e775101
Daniel Fontana <dfont110@gmail.com>
2019-09-26 16:50:09 -0500

    FEATURE: api routes working

---
+++ server/api/cubs.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const Cub = require('../db/cub');
+const router = express.Router();
+
+// GET all seasons
+
+router.get('/', async (req, res, next) => {
+  try {
+    res.json(await Cub.findAll());
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:year', async (req, res, next) => {
+  try {
+    const season = await Cub.findOne({
+      where: {
+        year: req.params.year,
+      },
+    });
+    season ? res.json(season) : res.sendStatus(404);
+  } catch (err) {
+    next(err);
+  }
+});
+module.exports = router;
